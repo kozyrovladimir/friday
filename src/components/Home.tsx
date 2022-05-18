247cadea337c93d695f5b6583b274e1b93c16be7
kozyrovladimir <kozyrovladimir@gmail.com>
2022-05-18 10:02:10 +0300

    feat: add display ping data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {cardsAPI} from "../api/axios";
-import {Simulate} from "react-dom/test-utils";
 
 const Home = () => {
+    const [info, setInfo] = useState<number | string>('');
+
     const onClickHandlerGet = () => {
+        setInfo('wait');
         cardsAPI.getPing().then(response => {
             console.log(response.data);
-        }).catch(error=> {console.log(error)})}
-
-    const onClickHandlerPost = () => {
-        cardsAPI.getPingPost().then(response => {
-            console.log(response.data);
+            setInfo(response.data.ping);
+        }).catch(error => {
+            console.log(error)
         })
     }
 
+    const onClickHandlerPost = () => {
+        setInfo('wait');
+        cardsAPI.getPingPost().then(response => {
+            console.log(response.data);
+            setInfo(response.data.ping);
+        }).catch(error => {
+            console.log(error)
+        });
+    }
     return (
         <div>
             <span>Home</span>
+            <div>
+                <span>{'Ping information: '}</span>
+                <span>{info ? info + '.' : 'no information.'}</span>
+            </div>
             <div>
                 <button onClick={onClickHandlerGet}>
                     get ping
